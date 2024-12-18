d3c1f8aab2d4d4ef509549a042f3040a5ea1a52d
Alen George <112187167+Alen545@users.noreply.github.com>
2024-12-18 17:04:51 +0530

    Update settings.py

--- BACKEND/TrainingWebsite/TrainingWebsite/settings.py
+++ BACKEND/TrainingWebsite/TrainingWebsite/settings.py
@@ -193,5 +193,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alengeorge1999@gmail.com' 
-EMAIL_HOST_PASSWORD = 'qjusgsgbldgbvqgd'
+EMAIL_HOST_USER = 'YOUR MAIL ID 
+EMAIL_HOST_PASSWORD = 'MAIL ID PASSWORD'
